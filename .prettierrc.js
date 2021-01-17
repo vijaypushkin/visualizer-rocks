371c5740ac083079d3920702d939bcfbf2cbf675
Vijay Pushkin <mail@vijaypushkin.me>
2021-01-17 16:34:40 +0530

    prettier added to the project

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'all',
+  jsxBracketSameLine: false,
+  tabWidth: 2,
+  printWidth: 80,
+}
