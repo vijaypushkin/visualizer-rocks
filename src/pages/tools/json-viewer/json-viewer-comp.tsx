f50e629d0622789f5136c1e41c0c26a2332453af
Vijay Pushkin <mail@vijaypushkin.me>
2021-01-20 19:44:44 +0530

    JSON edit 60% done

--- src/pages/tools/json-viewer/json-viewer-comp.tsx
+++ src/pages/tools/json-viewer/json-viewer-comp.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import {
   Badge,
+  Button,
   createStyles,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
   IconButton,
   makeStyles,
   Paper,
+  TextField,
   Theme,
   Typography,
 } from '@material-ui/core'
@@ -16,6 +22,7 @@ type TRenderNode<D = unknown> = (
   data: D,
   level: number,
   id: string,
+  handleEditClick: (data: unknown, path: string) => void,
   label?: string | number,
   shade?: 'black' | 'grey',
 ) => React.ReactChild
@@ -101,6 +108,32 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const JSONViewerComp: React.FC<{ data: unknown }> = ({ data }) => {
   const classes = useStyles()
+  const [edit, toggleEdit] = useState(false)
+  const [editData, setEditData] = useState<any>('')
+  const [editPath, setEditPath] = useState<string>('')
+
+  const handleEditClick = useCallback((data: unknown, path: string) => {
+    console.log(data, path)
+    toggleEdit(true)
+
+    let jsonStr = JSON.stringify(data, null, 2)
+    if (/^".*"$/.test(jsonStr)) {
+      jsonStr = jsonStr.slice(1, -1)
+    }
+    setEditData(jsonStr)
+    setEditPath(path)
+  }, [])
+
+  const handleCancelClick = useCallback(() => {
+    setEditData('')
+    setEditPath('')
+    toggleEdit(false)
+  }, [editData, editPath])
+
+  const handleSaveClick = useCallback(() => {
+    console.log(editData, editPath)
+    handleCancelClick()
+  }, [editData, editPath])
 
   if (data === null) {
     return (
@@ -111,24 +144,84 @@ const JSONViewerComp: React.FC<{ data: unknown }> = ({ data }) => {
   }
 
   return (
-    <Paper className={classes.root}>
-      <TreeView
-        className={classes.treeView}
-        defaultCollapseIcon={<ExpandMore />}
-        defaultExpandIcon={<ChevronRight />}
-      >
-        {renderData(data, 0, '0', null)}
-      </TreeView>
-    </Paper>
+    <>
+      <Paper className={classes.root}>
+        <TreeView
+          className={classes.treeView}
+          defaultCollapseIcon={<ExpandMore />}
+          defaultExpandIcon={<ChevronRight />}
+        >
+          {renderData(data, 0, '0', handleEditClick, null, 'grey')}
+        </TreeView>
+      </Paper>
+      {edit && (
+        <Dialog
+          open={edit}
+          onClose={() => toggleEdit(false)}
+          fullWidth={true}
+          maxWidth={'lg'}
+        >
+          <DialogTitle title={'Edit'} />
+          <DialogContent>
+            {editPath}
+            <TextField
+              autoFocus
+              margin={'dense'}
+              id={'name'}
+              type={'text'}
+              variant={'outlined'}
+              multiline={true}
+              rows={6}
+              value={editData}
+              onChange={ev => setEditData(ev.target.value)}
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCancelClick}>Cancel</Button>
+            <Button
+              color={'primary'}
+              variant={'contained'}
+              onClick={handleSaveClick}
+            >
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
+      )}
+    </>
   )
 }
 
-const renderData: TRenderNode = (data, level, id, label, shade = 'grey') => {
+const renderData: TRenderNode = (
+  data,
+  level,
+  id,
+  handleEditClick,
+  label,
+  shade = 'grey',
+) => {
   if (typeof data === 'object') {
     if (Array.isArray(data)) {
-      return renderArray(data, level, id, label, shade)
+      return renderArray(
+        data,
+        level,
+        id,
+
+        handleEditClick,
+        label,
+        shade,
+      )
     } else if (data !== null) {
-      return renderObject(data, level, id, label, shade)
+      return renderObject(
+        data,
+        level,
+        id,
+
+        handleEditClick,
+        label,
+        shade,
+      )
     }
   }
 
@@ -136,6 +229,7 @@ const renderData: TRenderNode = (data, level, id, label, shade = 'grey') => {
 
   return (
     <TreeItem
+      key={id}
       nodeId={id}
       label={
         <div className={'leaf-node'}>
@@ -144,10 +238,22 @@ const renderData: TRenderNode = (data, level, id, label, shade = 'grey') => {
             {dataType === 'string' ? `"${data}"` : `${data}`}
           </Typography>
           <div className={'icons-root'}>
-            <IconButton aria-label={'copy'}>
+            <IconButton
+              aria-label={'copy'}
+              onClick={ev => {
+                ev.preventDefault()
+                copyToClipboard(data)
+              }}
+            >
               <FileCopy />
             </IconButton>
-            <IconButton aria-label={'edit'}>
+            <IconButton
+              aria-label={'edit'}
+              onClick={ev => {
+                ev.preventDefault()
+                handleEditClick(data, id)
+              }}
+            >
               <Edit />
             </IconButton>
           </div>
@@ -161,11 +267,14 @@ const renderArray: TRenderNode<unknown[]> = (
   arrayData,
   level,
   id,
+
+  handleEditClick,
   label,
   shade = 'grey',
 ) => {
   return (
     <TreeItem
+      key={id}
       nodeId={id}
       className={'array-object-tree'}
       label={
@@ -177,10 +286,22 @@ const renderArray: TRenderNode<unknown[]> = (
           </Badge>
           &emsp; &ensp;
           <div className={'icons-root'}>
-            <IconButton aria-label={'copy'}>
+            <IconButton
+              aria-label={'copy'}
+              onClick={ev => {
+                ev.preventDefault()
+                copyToClipboard(arrayData)
+              }}
+            >
               <FileCopy />
             </IconButton>
-            <IconButton aria-label={'edit'}>
+            <IconButton
+              aria-label={'edit'}
+              onClick={ev => {
+                ev.preventDefault()
+                handleEditClick(arrayData, id)
+              }}
+            >
               <Edit />
             </IconButton>
           </div>
@@ -188,7 +309,7 @@ const renderArray: TRenderNode<unknown[]> = (
       }
     >
       {arrayData.map((datum, i) =>
-        renderData(datum, level + 1, `${id}.${i}`, i, 'grey'),
+        renderData(datum, level + 1, `${id}.${i}`, handleEditClick, i, 'grey'),
       )}
     </TreeItem>
   )
@@ -198,11 +319,13 @@ const renderObject: TRenderNode = (
   objectData,
   level,
   id,
+  handleEditClick,
   label,
   shade = 'grey',
 ) => {
   return (
     <TreeItem
+      key={id}
       nodeId={id}
       className={'array-object-tree'}
       label={
@@ -214,10 +337,22 @@ const renderObject: TRenderNode = (
           </Badge>
           &emsp; &ensp;
           <div className={'icons-root'}>
-            <IconButton aria-label={'copy'}>
+            <IconButton
+              aria-label={'copy'}
+              onClick={ev => {
+                ev.preventDefault()
+                copyToClipboard(objectData)
+              }}
+            >
               <FileCopy />
             </IconButton>
-            <IconButton aria-label={'edit'}>
+            <IconButton
+              aria-label={'edit'}
+              onClick={ev => {
+                ev.preventDefault()
+                handleEditClick(objectData, id)
+              }}
+            >
               <Edit />
             </IconButton>
           </div>
@@ -225,10 +360,28 @@ const renderObject: TRenderNode = (
       }
     >
       {Object.entries(objectData).map(([key, value], i) =>
-        renderData(value, level + 1, `${id}.${i}`, key, 'black'),
+        renderData(
+          value,
+          level + 1,
+          `${id}.${i}`,
+          handleEditClick,
+          key,
+          'black',
+        ),
       )}
     </TreeItem>
   )
+}
+
+const copyToClipboard = (data: unknown) => {
+  const container = document.createElement('textarea')
+  container.innerHTML = JSON.stringify(data, null, 2)
+
+  document.body.appendChild(container)
+  container.select()
+  document.execCommand('copy')
+
+  document.body.removeChild(container)
 }
 
 export default JSONViewerComp
